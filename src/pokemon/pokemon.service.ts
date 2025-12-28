6335c23a5e772edaa6c3a50cf010e1944e6a85c5
Hugo Tapia <hugotfdev0395@gmail.com>
2025-12-28 04:36:25 -0400

    finish seed and pagination

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -9,6 +9,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -27,9 +28,15 @@ export class PokemonService {
     }
   }
 
-  async findAll() {
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
     try {
-      const pokemons = await this.pokemonModel.find();
+      const pokemons = await this.pokemonModel
+        .find()
+        .limit(limit)
+        .skip(offset)
+        .sort({ no: 1 })
+        .select('-__v');
       if (!pokemons) throw new NotFoundException('No pokemons found');
       return pokemons;
     } catch (error) {
